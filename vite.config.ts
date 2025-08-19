40e05b6128cd060a6511d30d83bce406e5ea1b37
Ilian <ilian@teamgeek.io>
2025-08-19 16:33:35 +0200

    fix: set NODE_ENV so vite doesn't include development packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: { 'process.env.NODE_ENV': '"production"' },
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/entry.js'),
